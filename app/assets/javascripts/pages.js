ca2ff6c89b954b35e2aa608899b1b6cc99c8af85
Peter Jang <peter.x.jang@gmail.com>
2016-05-05 16:21:39 -0500

    simple firebase chat example

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,22 @@
-/* global angular */
+/* global angular, Firebase */
 (function() {
-  angular.module('app').controller('pagesCtrl', function($scope) {
-    $scope.message = "hello";
+  angular.module('app').controller('pagesCtrl', function($scope, $timeout) {
+    var myDataRef = new Firebase('https://incandescent-fire-3510.firebaseio.com/');
+    
+    $scope.setup = function() {
+      $scope.messages = [];
+      myDataRef.on('child_added', function(snapshot) {
+        $timeout(function() {
+          $scope.messages.push(snapshot.val());
+        });
+      });
+    };
+
+    $scope.createMessage = function(inputText) {
+      myDataRef.push({name: "Peter Jang", text: inputText});
+      $scope.newMessageText = '';
+    };
+
+    window.$scope = $scope;
   });
 })();
